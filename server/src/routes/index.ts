6cf76ffa9d1304c5542cb91cd3b589ce71be172f
aryanc381 <chauhanaryan381@gmail.com>
2026-07-10 16:49:58 +0530

    feat(db): added prisma configuration.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,9 @@
 import express, { Router } from 'express';
+import signupRouter from './auth/signup.route.js';
+import loginRouter from './auth/login.route.js';
 
 const router: Router = express.Router();
+
+router.use('/auth', signupRouter, loginRouter);
 
 export default router;
